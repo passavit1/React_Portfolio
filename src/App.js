f8418e46ba404fd5e8eb58003de90a970803d4fc
Wizat <passavit1@gmail.com>
2023-04-11 15:39:42 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainPage } from "./Pages/index";
+
 function App() {
-  return <div>hello my new project PortFolio</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Pages/index.js
@@ -0,0 +1,8 @@
+import About from "./About/About.jsx";
+import CareerPath from "./CareerPath/CareerPath.jsx";
+import Contact from "./Contact/Contact.jsx";
+import Portfolio from "./Portfolio/Portfolio.jsx";
+import Skills from "./Skills/Skills.jsx";
+import MainPage from "./Main/Main.jsx";
+
+export { About, CareerPath, Contact, Portfolio, Skills, MainPage };
