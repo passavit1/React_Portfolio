e556e7ab62d8c23813b956d0a76c503e3528866c
Passavit_Gunn <passavit1@gmail.com>
2023-04-18 00:54:09 +0700

    set position

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -2,8 +2,35 @@ import { About, CareerPath, Contact, Portfolio, Skills } from "../index";
 import { Tabs } from "antd";
 import styled from "styled-components";
 
-const StyledTab = styled(Tabs)`
-  color: red;
+const StyledContainer = styled.div`
+  height: 100vh;
+  padding: 10px;
+  background-color: #d8dee8;
+
+  .ant-tabs {
+    height: 100%;
+
+    .ant-tabs-nav {
+      width: 30%;
+    }
+
+    .ant-tabs-tab {
+      height: 20%;
+      display: flex;
+      text-align: center;
+      justify-content: center;
+      border: 1px solid red;
+    }
+
+    .ant-tabs-ink-bar {
+      display: none;
+    }
+
+    .ant-tabs-content-holder {
+      border: 1px solid green;
+      background-color: #c1cfe8;
+    }
+  }
 `;
 
 const MainPage = () => {
@@ -37,15 +64,15 @@ const MainPage = () => {
 
   return (
     <>
-      <div>
-        <StyledTab tabPosition={"left"}>
+      <StyledContainer>
+        <Tabs tabPosition={"left"}>
           {tabs.map(({ key, title, content }) => (
             <Tabs.TabPane key={key} tab={title}>
               {content}
             </Tabs.TabPane>
           ))}
-        </StyledTab>
-      </div>
+        </Tabs>
+      </StyledContainer>
     </>
   );
 };
