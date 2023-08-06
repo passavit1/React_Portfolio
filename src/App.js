e224218cfa981b2a88ae13ae0821a41d7582a9b3
Wizat <passavit1@gmail.com>
2023-08-06 16:03:15 +0700

    start over

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MainPage } from "./Pages/index";
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/Pages/index.js
+++
@@ -1,8 +0,0 @@
-import About from "./About/About.jsx";
-import CareerPath from "./CareerPath/CareerPath.jsx";
-import Contact from "./Contact/Contact.jsx";
-import Portfolio from "./Portfolio/Portfolio.jsx";
-import Skills from "./Skills/Skills.jsx";
-import MainPage from "./Main/Main.jsx";
-
-export { About, CareerPath, Contact, Portfolio, Skills, MainPage };
